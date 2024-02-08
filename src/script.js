c4fbd3ebb5e6c20b32f4a95344a31e001944ba1d
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-02-08 12:32:28 +0200

    javascript working

--- src/script.js
+++ src/script.js
@@ -1,6 +1,19 @@
 /*console.log(fetch('https://api.dictionaryapi.dev/api/v2/entries/en/hello'))
 
-response.data[0].meanings[0].definitions[0].definition*/
+response.data[0].meanings[0].definitions[0].definition
+
+function mySearch() {
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    let word = document.getElementById("search");
+    document.getElementById("word").innerHTML = `${word}`
+
+   
+}
+*/
+
+let url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+axios.get(url).then(showWord);
 
 function showWord(response) {
     let h2 = document.querySelector("h2")
@@ -11,11 +24,39 @@ function showWord(response) {
     let definition = response.data[0].meanings[0].definitions[0].definition
     h3.innerHTML = `${definition}`
 
-    console.log(response.data[0])
+    let audio = document.querySelector("audio")
+    let phonetics = response.data[0].phonetics[0].audio
+    audio.innerHTML = `${phonetics}`
+
+    console.log(response.data[0].phonetics[0])
 }
 
-let url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
-axios.get(url).then(showWord);
+
+
+/*function submitButton(event) {
+    event.preventDefault()
+    let word = document.querySelector("#searchWord")
+
+    let h2 = document.querySelector("h2")
+    h2.innerHTML = `${word.value}`
+
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`;
+
+    axios.get(url).then(showWord);
+}
+
+let wordForm = document.querySelector("#searchBar")
+wordForm.addEventListener("submit", submitButton)*/
+
+
+
+
+
+
+
+
+
+
 
 
 
